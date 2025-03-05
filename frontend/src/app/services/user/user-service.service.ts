96e386fcead5f8b5747903f179c5b539e18b4bea
AntoineCdrc <acouderc@et.esiea.fr>
2025-03-05 16:16:18 +0100

    debut login front

--- frontend/src/app/services/user/user-service.service.ts
+++ frontend/src/app/services/user/user-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Observable } from 'rxjs';
 import { User } from '../../interfaces/user';
+import { HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ import { User } from '../../interfaces/user';
 export class UserServiceService {
 
   public static USER_URL = ApiService.DEFAULT_URL + 'users'
+  jwtHelper: any;
 
   constructor(
     private api: ApiService,
@@ -18,4 +20,35 @@ export class UserServiceService {
     console.log(UserServiceService.USER_URL)
     return this.api.sendGetRequest(UserServiceService.USER_URL, null);
   }
+
+  public login(email: string, password: string): Observable<void> {
+    let params: HttpParams = new HttpParams({
+      fromObject: {
+        email: email,
+        password: password
+      }
+    });
+    let obs: Observable<HttpResponse<string>> = this.api.sendPostRequest(ApiService.DEFAULT_URL + '/login', params, null);
+    return new Observable<void>((observer) => {
+      obs.subscribe({
+        next: (data: HttpResponse<string>) => {
+          let headers: HttpHeaders = data.headers;
+          let jwt: string | null = headers.get("Authorization");
+  
+          if (jwt) {
+            localStorage.setItem("token", jwt);
+          }
+          if (jwt) {
+            console.log("Connexion réussie");
+          }  
+          observer.next();
+          observer.complete();
+        },
+        error: (err) => {
+          observer.error(err);
+        }
+      });
+    });
+  }
 }
+
